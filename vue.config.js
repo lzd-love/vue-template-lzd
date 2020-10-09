e285dc76718b72d16e790122c70251a722e8fd34
Lzd <1023478655@qq.com>
2020-10-09 10:04:36 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -64,6 +64,12 @@ module.exports = {
         secure: false, //是否使用https加密 如果后台是非安全证书，关闭加密
         changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
         pathRewrite: { "^/api": "" } //这里重写路径
+      },
+      "/ws": {
+        target: "ws://10.129.41.91:7090", //目标地址  使用时 baseUrl = "ws://" + location.host + "/ws"; 加上本地服务器网址
+        ws: true, //// 是否启用websockets
+        changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
+        pathRewrite: { "^/ws": "" } //这里重写路径
       }
     }
   },
